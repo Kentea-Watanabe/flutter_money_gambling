189752a7a2650e2b1e9b9133fd0391760633298e
kentawatanabe <kenta.watanabe1023@gmail.com>
2022-12-18 21:07:58 +0900

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -35,13 +35,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 1;
-
+  var _text = '';
+  var String_count = "hoge";
+  final _editController = TextEditingController();
   void _incrementCounter() {
     setState(() {
       var random = math.Random();
-      if (_counter == 0) {
-        _counter = 1;
-      }
+      // 0になったら永遠と0円
+      // if (_counter == 0) {
+      //   _counter = 1;
+      // }
       _counter *= random.nextInt(10);
     });
   }
@@ -57,7 +60,22 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '$_counter円',
+              'Write your name and click UP.',
+              style: TextStyle(
+                fontSize: 44,
+                height: 2,
+              ),
+            ),
+            new TextField(
+              controller: _editController,
+              decoration: InputDecoration(
+                hintText: 'please give me a nickname',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Text(
+              '所持金 : $_counter円',
               style: Theme.of(context).textTheme.headline4,
             ),
             FloatingActionButton(
@@ -67,9 +85,26 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context).pushNamed("/result", arguments: _counter);
+                // 入力フォームの値を取得
+                _text = _editController.text;
+                // int型を文字型に変換
+                String String_count = _counter.toString();
+                // 結果表示のため、result_val = ニックネーム, 所持金とする
+                var result_val = "$_text , $String_count";
+                // 所持金を初期化
+                _counter = 1;
+                // 入力フォームを初期化
+                _editController.clear();
+                Navigator.of(context)
+                    .pushNamed('/result', arguments: result_val);
               },
-              child: Text('END'),
+              child: Text(
+                'END',
+                style: TextStyle(
+                  fontSize: 44,
+                  height: 1.5,
+                ),
+              ),
             )
           ],
         ),
@@ -78,14 +113,20 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+var get_result = <String>[];
+
 class ResultPage extends StatelessWidget {
   int result = 9999;
+  String name = "hoge";
   Object? args;
   @override
   Widget build(BuildContext context) {
     if (args == null) {
       args = ModalRoute.of(context)!.settings.arguments;
-      result = args as int; //Object型なので型を指定する
+      // result = args as int; //Object型なので型を指定する
+      // list.add(result);
+      name = args as String; //Object型なので型を指定する
+      get_result.add(name);
     }
     return Scaffold(
       appBar: AppBar(
@@ -95,13 +136,15 @@ class ResultPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '$result円',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            // for (final list_ in list)
+            for (final result_ in get_result)
+              Text(
+                '$result_円',
+                style: Theme.of(context).textTheme.headline4,
+              ),
             TextButton(
               onPressed: () {
-                Navigator.popUntil(context, (route) => route.isFirst);
+                Navigator.of(context).pop();
               },
               child: Text('Go Back Home'),
             )
